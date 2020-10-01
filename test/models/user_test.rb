f1acebd5d95a10cd2b5a2b500a4e44bb7fdd7f95
平沼 智之 <tomoyuki.hiranuma110664@e-seikatsu.co.jp>
2020-10-01 23:29:11 +0900

    [WIP]user model test 記述

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,12 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @user1 = User.new(name: "name1", password: "password1")
+  end
+
+  test "should be valid" do
+    assert @user1.valid?
+  end
 end
