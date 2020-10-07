e378cb0c857dc4d2c24326b5f7008e25e975ed9e
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2020-10-07 17:58:14 +0900

    正常ケースがうまくいかない

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,5 +1,7 @@
 class Chat < ApplicationRecord
   belongs_to :room
   belongs_to :user
-  validates :text, presence: true
+  validates :user_id, presence: true
+  validates :room_id, presence: true
+  validates :text, presence: true, length:{ maximum:255 }
 end
--- test/models/chat_test.rb
+++ test/models/chat_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class ChatTest < ActiveSupport::TestCase
-  test "should be valid" do
-    assert true
+  def setup
+    @chat_test= chats(:one)
   end
+  test "should be valid" do
+    assert @chat_test.valid?
+    assert_equal("test",@chat_test.text)
+  end
+  test "text should not be too long" do
+    @chat_test.text = "a"*256
+    assert_not @chat_test.valid?
+  end
+  test "text should be  long" do
+    @chat_test.text = "aafdsf"
+    assert @chat_test.valid?
+    
+  end
+  test "text should be present" do
+    @chat_test.text=""
+    assert_not @chat_test.valid?
+  end
+
+
 end
