9f2a824d8ab56a50fdb3ebfa85037b3ff19fc7e8
平沼 智之 <tomoyuki.hiranuma110664@e-seikatsu.co.jp>
2020-10-08 20:54:21 +0900

    user model テスト中

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,14 +8,18 @@ class UserTest < ActiveSupport::TestCase
   def setup
     @user1 = User.new(name: "hiranuma", password: "1234")
     @user2 = User.new(name: "asada", password: "1234")
+    print @user
+    # @user1.follow(@user2)
+    # @user = User.find_by(name: "hiranuma")
   end
 
   test "should be valid" do
     assert @user1.valid?
     assert @user2.valid?
+    # assert @user.valid?
   end
 
-  test "can be follow" do
-    # @user1.follow(@user2)
-  end
+  # test "can be follow?" do
+  #   assert @user1.following?(@user2)
+  # end
 end
