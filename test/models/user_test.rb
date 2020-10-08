b45f3a878edc997f54ca3183e26454bfec148938
平沼 智之 <tomoyuki.hiranuma110664@e-seikatsu.co.jp>
2020-10-08 19:41:58 +0900

    test環境できたかも

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,10 +6,16 @@ class UserTest < ActiveSupport::TestCase
   # end
 
   def setup
-    @user1 = User.new(name: "name1", password: "password1")
+    @user1 = User.new(name: "hiranuma", password: "1234")
+    @user2 = User.new(name: "asada", password: "1234")
   end
 
   test "should be valid" do
     assert @user1.valid?
+    assert @user2.valid?
+  end
+
+  test "can be follow" do
+    # @user1.follow(@user2)
   end
 end
