2dcc56460e5563be8332a174e7b6c4a741f24972
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2020-10-28 22:00:43 +0900

    部屋に入室した際にbroadcastで全員に入室を伝える

--- app/controllers/api/v1/room_users_controller.rb
+++ app/controllers/api/v1/room_users_controller.rb
@@ -9,22 +9,25 @@ module Api
 			jwt_authenticate 
 			# ルームに入室しているユーザ一覧を取得(入室した順番に取得)
 
-			#Kyosuke Yokota
 			def index 
-				users = User.where(room_id: @current_user.room_id).order(updated_at: :desc)#rikuiwasaki
-				render status:200, json: { status: 'SUCCESS', message: 'Loaded posts', data: { users: users } }
+				users = User.where(room_id: @current_user.room_id).order(updated_at: :desc)
+				render status:200, json: { message: 'Loaded posts', data: { users: users } }
 			end
-			#Kyosuke Yokota
 			def create
 				if @current_user.update_attribute(:room_id, room_id_params[:room_id])
-					render status:201, json: { status: "SUCCESS", data: { user: @current_user } }
+					info = { type:"add",user:{id:@current_user.id,name:@current_user.name}}
+					users = User.where(room_id: room_id_params[:room_id]).select(:id,:name)
+					render status:201, json: { data: { users: users } }
+					RoomChannel.broadcast_to("room_#{room_id_params[:room_id]}",info)
 				else
-					render status:500, json: { status: "ERROR", data: { error: @current_user.errors } }
+					render status:500, json: { data: { error: @current_user.errors } }
 				end
 			end
 
 			def leave
 				if @current_user.update_attribute(:room_id, nil)
+					
+					RoomChannel.broadcast_to("room_#{room_id_params[:room_id]}",info)
 					render status:204, json: { status: "SUCCESS", data: {} }
 				else
 					render status:500, json: { status: "ERROR", data: { error: @current_user.errors } }
