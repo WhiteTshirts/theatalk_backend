e6b1fc27efe75347b35da62c1cb6377ef3366564
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2020-10-30 14:46:25 +0900

    jwt_tokenをログイン時に渡して欲しいので修正

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -8,7 +8,7 @@ module Api
         if @current_user && @current_user.authenticate(user_params[:password])
           jwt_token = encode(@current_user.id)
           response.headers['X-Authentication-Token'] = jwt_token
-          render status:200, json: { data: { user: @current_user } }
+          render status:200, json: { data: { user: @current_user,token: jwt_token } }
         else
           render status:401
         end
