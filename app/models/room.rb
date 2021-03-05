661286f7f7c5510b07464527d68841825f72e781
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2021-03-06 00:06:11 +0900

    added RoomUser

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,9 +1,10 @@
 class Room < ApplicationRecord
     has_many :chats #rikuiwasaki
     has_many :rooms_tags
+    has_many :users, through: :room_users
+    has_many :room_users
     #Kyosuke Yokota
     has_and_belongs_to_many :tags
-
     validates :admin_id, presence: true
     validates :youtube_id, presence: true
     validates :name, presence: true
---
+++ spec/factories/room_users.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :room_user do
+    user { nil }
+    room { nil }
+  end
+end
