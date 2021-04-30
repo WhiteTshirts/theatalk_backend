98b579ab6f9dab52445b1292e796ac7c413fe151
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-04-30 11:55:43 +0900

    refactor image controller

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -12,11 +12,15 @@ module Api
         images = [
           { 'id': 0, 'path': "public/images/avaters/pose_pien_uruuru_man.png" }
         ]
-        image = images[params[:id].to_i]
-        image_path = image[:path]
-        image_data = File.open(image_path).read
-        image_base64 = Base64.encode64(image_data)
+        image_path = images[params[:id].to_i][:path]
+        image_base64 = convert_base64(image_path)
         render status: 200, json: image_base64
+      end
+
+      private
+
+      def convert_base64(path)
+        Base64.encode64(File.open(path).read)
       end
 
     end
