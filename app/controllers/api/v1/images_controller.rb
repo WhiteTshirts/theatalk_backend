e89a42bf286e9df30370aa0a1d5f1f266458545d
tomoyuki-hiranuma <numanumatom@gmail.com>
2021-04-30 11:50:36 +0900

    fix unnecessary program

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -2,16 +2,13 @@ module Api
   module V1
     class ImagesController < ApplicationController
       require 'base64'
-      require 'uri'
 
-      # 画像のid返す
       def index
         images = [ { 'id': 0, 'path': "public/images/avaters/pose_pien_uruuru_man.png" } ]
         render status: 200, json: images
       end
 
       def show
-        # image dataをidで取得
         images = [
           { 'id': 0, 'path': "public/images/avaters/pose_pien_uruuru_man.png" }
         ]
@@ -19,7 +16,6 @@ module Api
         image_path = image[:path]
         image_data = File.open(image_path).read
         image_base64 = Base64.encode64(image_data)
-        # Bodyにbaseの内容を返す
         render status: 200, json: image_base64
       end
 
