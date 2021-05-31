9ddb4db48b6dea0c70cc12b48670d036310a02e3
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2021-05-31 23:25:20 +0900

    422と409の関数をついか

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::API
   include JwtAuthenticator
   rescue_from StandardError,:with => :error_500
-  def error_500(e)
-    render status:500,json:{errors:"例外が発生しました"}
+  def error_500(message="Server Error")
+    render status:500,json:{errors:message}
+  end
+  def error_422(message="Unprocessable Entity")
+    render staus:422,json:{errors:message}
+  end
+  def error_409(message="Conflict")
+    render status:409,json:{errors:message}
   end
 end
