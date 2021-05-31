06801038c4edfff9b8dbc3944c78c6cf0ea27742
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2021-05-31 22:45:59 +0900

    Chatの例外処理をまとめた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::API
   include JwtAuthenticator
+  rescue_from StandardError,:with => :error_500
+  def error_500(e)
+    render status:500,json:{errors:"例外が発生しました"}
+  end
 end
