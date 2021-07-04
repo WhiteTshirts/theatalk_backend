b46ee1aa5c1b9916de078418dc84b839a170f5a4
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2021-07-04 11:45:54 +0900

    useridからavaterを取得するapiの追加

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,14 @@ module Api
   module V1
     class UsersController < ApplicationController
       jwt_authenticate except: [:create]
-      
+      @@images = [
+        { 'id': 0, 'path': "public/images/avaters/dog.jpeg" },
+        { 'id': 1, 'path': "public/images/avaters/cat.jpeg" },
+        { 'id': 2, 'path': "public/images/avaters/whale.jpeg" },
+        { 'id': 3, 'path': "public/images/avaters/sheep.jpeg" },
+        { 'id': 4, 'path': "public/images/avaters/penguin.jpeg" },
+        { 'id': 5, 'path': "public/images/avaters/dragon.jpeg" }
+      ]
       def index 
         users = User.all
         render status: 200, json: users, user: @current_user, scope: :detail
@@ -21,6 +28,12 @@ module Api
         render status: 201, json: { user: @user, token: jwt_token  }
 
       end
+      def avater
+        avater = Avater.find_by(user_id:params[:id])
+        avater_id = avater.present?? avater.id : 0
+        image_path = @@images[avater_id][:path]
+        render status: 201, json: { avater: { id: avater_id, image: convert_base64(image_path) } }
+      end
 
       def update
         @current_user.update_attributes!(user_params)
@@ -32,7 +45,9 @@ module Api
       def user_params
         params.require(:user).permit(:name, :profile, :room_id, :password)
       end
-
+      def convert_base64(path)
+        Base64.encode64(File.open(path).read)
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace 'api' do
     namespace 'v1' do
-      resources :users
+      resources :users do
+        member do
+          get 'avater'
+        end
+      end
       resources :images
       resources :room_histories
       resources :dms do
